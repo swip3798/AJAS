10d1d818428504b078bbcbc7acd6476a1864166f
Christian <christian.sw@hotmail.de>
2019-06-07 14:46:34 +0200

    Add unittests for api and Serializer

---
+++ unittests.py
@@ -0,0 +1,39 @@
+import unittest
+from AJAS import Api, Serializer
+
+class Unitests_Api(unittest.TestCase):
+    def test_init(self):
+        api = Api()
+        self.assertEqual(api.resolvers_get, {}, "Resolver for GET is not initial")
+        self.assertEqual(api.resolvers_post, {}, "Resolver for POST is not initial")
+    
+    def test_add_get_resolver(self):
+        self.api.add_get_resolver("/testpath", lambda x, y: {"test": 1})
+        self.assertEqual(len(self.api.resolvers_get), 1, "GET resolver was not added")
+        self.assertEqual(list(self.api.resolvers_get.keys()), ["/testpath"], "Path is wrong")
+        self.assertEqual(self.api.resolvers_get["/testpath"](1, 1), {"test": 1}, "Callback function does not return expected value")
+
+    def test_add_post_resolver(self):
+        self.api.add_post_resolver("/testpath", lambda x, y: {"test": 1})
+        self.assertEqual(len(self.api.resolvers_post), 1, "POST resolver was not added")
+        self.assertEqual(list(self.api.resolvers_post.keys()), ["/testpath"], "Path is wrong")
+        self.assertEqual(self.api.resolvers_post["/testpath"](1, 1), {"test": 1}, "Callback function does not return expected value")
+
+    def test_callback_get(self):
+        self.api.add_get_resolver("/testpath", lambda x, y: {"test": 1})
+        jsonstr = self.api.get_callback("/testpath")
+        self.assertEqual(jsonstr, '{"test": 1}', "JSON string is wrong")
+    
+    def setUp(self):
+        self.api = Api()
+
+    def tearDown(self):
+        self.api = None
+
+class SerializerTests(unittest.TestCase):
+    def test_serialize(self):
+        s = Serializer()
+        self.assertEqual(s.serialize({"test": 1}), '{"test": 1}')
+
+if __name__ == '__main__':
+    unittest.main()
