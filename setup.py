3511d71083b6c6b6ab96cecd3ecf8ff7039f202d
Christian <christian.sw@hotmail.de>
2019-06-19 19:26:58 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("reqirements.txt") as f:
+with open("requirements.txt") as f:
     requirements = f.readlines()
     print(requirements)
 
